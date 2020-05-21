b7f8af6ab84cc9119552ce2e0afdac5b2eb519d5
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-05-21 20:30:42 -0300

    done

--- server.js
+++ server.js
@@ -14,11 +14,14 @@ app.use("/", (req, res) => {
   res.render("index.html");
 });
 
+let messages = [];
+
 io.on("connection", (socket) => {
   console.log(`socket conectado: ${socket.id}`);
 
   socket.on("sendMessage", (data) => {
-    console.log(data);
+    messages.push(data);
+    socket.broadcast.emit("receivedMessage", data);
   });
 });
 
